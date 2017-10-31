f3a4deffc89d6bac6aecf4dd2f4536e04559a725
Nandu kmg <nandukrishnan55555@gmail.com>
2017-11-01 00:30:05 +0530

    menuvum gradient athalle heroism

--- js/main.js
+++ js/main.js
@@ -1328,26 +1328,24 @@
 })(window);
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
+	var w = window.innerWidth;
+	console.log(w);
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0)";
+    if(w<=480){
+    	document.getElementById("mySidenav").style.width = "100vw";
+        document.getElementById("main").style.marginLeft = "100vw";
+    }	
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
+	var w = window.innerWidth;
+	console.log(w);
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
